7137b6a1a70063b74563bed55568db3e9da61b8a
TESTID0000x163 <ddolddol9906@icloud.com>
2022-01-01 17:40:00 +0900

    add: BBGT, Buy-BBGT

---
+++ src/contracts/BBGT.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
+
+contract BBGT is ERC20 {
+    constructor(uint256 amount) ERC20("Bbagutte Token", "BBGT") { 
+        _mint(msg.sender, amount);
+    }
+
+}
---
+++ src/contracts/Buy-BBGT.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.0;
+
+interface Token {
+  function balanceOf(address _owner) external view returns (uint256); 		
+  function transfer(address to, uint256 value) external returns (bool);
+
+  event Transfer(address indexed from, address indexed to, uint256 value);
+}
+
+contract BuyBBGT {
+  Token public tokenInstance;
+  address public owner;
+
+  constructor(address _tokenAddress) {
+    tokenInstance = Token(_tokenAddress);
+    owner = msg.sender;
+  }
+
+  modifier onlyOwner() {
+    require(msg.sender == owner);
+    _;
+  }
+
+  function getBalance() public view returns(uint256) {
+    return tokenInstance.balanceOf(address(this));
+  }
+
+  function transferEthToOnwer() onlyOwner public {
+    payable(owner).transfer(address(this).balance);
+  }
+
+  function transferTokenToOnwer() onlyOwner public {
+    tokenInstance.transfer(address(owner), tokenInstance.balanceOf(address(this)));
+  }
+
+  function kill() onlyOwner public {
+		transferTokenToOnwer();
+    selfdestruct(payable(owner));
+  }
+
+  function buy() payable public{ 
+		require(msg.value > 0, "amount must bigger than ZERO");
+    uint256 amountTobuy = msg.value;
+    uint256 dexBalance = tokenInstance.balanceOf(address(this));
+    require(amountTobuy > 0, "You need to send some ether");
+    require(amountTobuy <= dexBalance, "Not enough tokens in the reserve");
+    tokenInstance.transfer(msg.sender, amountTobuy * 100);
+	}
+    
+}
