4452e947b6070fd6973d0fc4207be8eac363121f
Sueun Cho <57546981+sueun-dev@users.noreply.github.com>
2022-01-09 09:55:49 +0900

    Update BBGT.sol

--- src/contracts/BBGT.sol
+++ src/contracts/BBGT.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
-//import openzeppelin
-// Developer 
+// import openzeppelin
+// Developer : Sueun-dev, junha-ahn 
+
 pragma solidity ^0.8.0;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC20/ERC20.sol";
@@ -9,5 +10,4 @@ contract BBGT is ERC20 {
     constructor(uint256 amount) ERC20("Bbagutte Token", "BBGT") { 
         _mint(msg.sender, amount);
     }
-
 }
