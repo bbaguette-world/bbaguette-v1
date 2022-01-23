1c224c53494f796f78950c69a771067b3bbc0e3c
junha-ahn <junha.ahn.dev@gmail.com>
2022-01-23 15:43:47 +0900

    fix: double extents.

--- src/contracts/NFT/ERC721/BBGTv1.sol
+++ src/contracts/NFT/ERC721/BBGTv1.sol
@@ -3,11 +3,10 @@ pragma solidity ^0.8.0;
 
 import "../../openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "../../openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import "../../openzeppelin/contracts/utils/Context.sol";
 import "../../openzeppelin/contracts/utils/Counters.sol";
 import "../../openzeppelin/contracts/access/Ownable.sol";
 
-contract BBGTv1 is ERC721, ERC721Enumerable, Context, Ownable {
+contract BBGTv1 is ERC721, ERC721Enumerable, Ownable {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
